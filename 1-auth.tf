1807103b0ebabd371ef362dd239fe9ec071a9d68
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-01-12 12:54:25 -0800

    update auth

--- 1-auth.tf
+++ 1-auth.tf
@@ -17,12 +17,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket  = "tf-backend-4357232"                 # Name of the S3 bucket
-    key     = "static-website-cors-011225.tfstate" # The name of the state file in the bucket
-    region  = "us-east-1"                          # Use a variable for the region
-    encrypt = true                                 # Enable server-side encryption (optional but recommended)
-  }
+ 
 }
 
 provider "aws" {
